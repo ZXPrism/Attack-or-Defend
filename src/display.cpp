bcc585ca21e3c3756fc9a27ac9af9d7b6c84ac72
ZXPrism <2897544036@qq.com>
2024-08-05 12:04:23 +0800

    update

--- src/display.cpp
+++ src/display.cpp
@@ -8,7 +8,7 @@ void DISPLAY::logo()
 {
 	std::cout<<"               "<<"----------Attack or Defend I [Console 32bit]----------"<<std::endl;
 	Sleep(1000);
-	std::cout<<"                    "<<"-------------By Zhou Leyi-------------"<<std::endl;
+	std::cout<<"                    "<<"-------------By ZXPRISM-------------"<<std::endl;
 	Sleep(1000);
 	std::cout<<"                        "<<">>>>>>>>>"<<this->version<<"<<<<<<<<"<<std::endl;
 	Sleep(1500);
@@ -19,22 +19,22 @@ void DISPLAY::draw(AI &a,PLAYER &p)
 {
 	std::system("cls");
 	std::cout<<"********************************************************"<<std::endl;
-	std::cout<<"ÎÒ·½"<<"                         "<<"µÐ·½"<<std::endl;
+	std::cout<<"ï¿½Ò·ï¿½"<<"                         "<<"ï¿½Ð·ï¿½"<<std::endl;
 	for(int n=1;n<=a.Aamount;n++)
 	{
 		if(n>p.Pamount)
 		{
 			std::cout<<std::endl; 
 			std::cout<<"                                   ""["<<n<<"]"<<a.Adata[3*n-2]<<std::endl;
-			std::cout<<"                                   "<<"ÑªÁ¿:"<<a.Adata[3*n]<<std::endl;
+			std::cout<<"                                   "<<"Ñªï¿½ï¿½:"<<a.Adata[3*n]<<std::endl;
 		}
 		else
 		{
 		std::cout<<std::endl;
 		std::cout<<"["<<n<<"]"<<p.Pdata[3*n-2]<<std::flush;
 		std::cout<<"                      ""["<<n<<"]"<<a.Adata[3*n-2]<<std::endl;
-		std::cout<<"ÑªÁ¿:"<<p.Pdata[3*n]<<std::flush;
-		std::cout<<"                         "<<"ÑªÁ¿:"<<a.Adata[3*n]<<std::endl;
+		std::cout<<"Ñªï¿½ï¿½:"<<p.Pdata[3*n]<<std::flush;
+		std::cout<<"                         "<<"Ñªï¿½ï¿½:"<<a.Adata[3*n]<<std::endl;
 	    }
 	}
 	std::cout<<"********************************************************By Zhou Leyi"<<std::endl; 
